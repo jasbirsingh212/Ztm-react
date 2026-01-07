1c71e5dbf02df3ec002ebf4d682f80d8c8497892
Jasbir Singh <jasbir212singh@gmail.com>
2026-01-07 20:26:55 +0530

    removed usestate and console log

--- jasbir-singh-project/src/App.jsx
+++ jasbir-singh-project/src/App.jsx
@@ -1,12 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log("Jasbir Singh");
-
   return <div>Hello World!</div>;
 }
 
