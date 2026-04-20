e1a1767b190d7e72065cd0efca0a156e65e05629
Jasbir Singh <jasbir212singh@gmail.com>
2026-04-20 19:19:08 +0530

    added category container and category items

--- crown-clothing/src/App.jsx
+++ crown-clothing/src/App.jsx
@@ -1,11 +1,9 @@
+import CategoryContainer from "./components/CategoryContainer";
+
 const App = () => {
   return (
-    <main>
-      <h1>Crown Clothing</h1>
-      <p>
-        Welcome to Crown Clothing, your one-stop shop for all your fashion
-        needs!
-      </p>
+    <main className="App">
+      <CategoryContainer />
     </main>
   );
 };
