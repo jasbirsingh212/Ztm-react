4e9cd440211410dd58900a2bf4c4b3dd928138c0
Jasbir Singh <jasbir212singh@gmail.com>
2026-04-20 18:33:36 +0530

    added Open Sans font to the project

--- crown-clothing/src/App.jsx
+++ crown-clothing/src/App.jsx
@@ -2,6 +2,10 @@ const App = () => {
   return (
     <main>
       <h1>Crown Clothing</h1>
+      <p>
+        Welcome to Crown Clothing, your one-stop shop for all your fashion
+        needs!
+      </p>
     </main>
   );
 };
