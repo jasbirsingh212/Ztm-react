25f7f965389d78a27fe4d46e157f4b8918fd1e02
Jasbir Singh <jasbir212singh@gmail.com>
2026-04-21 12:48:57 +0530

    completed navbar

--- crown-clothing/src/components/NavBar/NavBar.jsx
+++ crown-clothing/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { Link } from "react-router";
+import "./NavBar.styles.scss";
+import logo from "../../assets/crown.svg";
+import cartIcon from "../../assets/shopping-bag.svg";
 
 const NavBar = () => {
   return (
-    <div className="nav-bar">
-      <h1>NavBar Component</h1>
-    </div>
+    <nav className="nav-bar">
+      <Link to="/" className="logo">
+        <img src={logo} alt="Logo" />
+      </Link>
+      <div className="nav-links">
+        <Link to="/shop" className="nav-link">
+          Shop
+        </Link>
+        <Link to="/contact" className="nav-link">
+          Contact
+        </Link>
+        <Link to="/signin" className="nav-link">
+          Sign In
+        </Link>
+        <Link to="/cart" className="nav-link cart-icon">
+          <img src={cartIcon} alt="Cart" />
+        </Link>
+      </div>
+    </nav>
   );
 };
 
